7897aa730e817ce2ffb49693f7bae5f48883e41f
carolynhudson <ashleyatheart@gmail.com>
2025-02-26 01:13:58 -0800

    Replaced backslashes in some paths.

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ def main():
     print("hello world")
     purge_contents("./public")
     copy_folder("./static", "./public")
-    generate_page(".\content\index.md", ".\template.html", ".\public\index.html")
+    generate_page("./content/index.md", "./template.html", "./public/index.html")
 
 
 main()
