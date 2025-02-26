838167a9f5a462d502787b1c50a27dace22df206
carolynhudson <ashleyatheart@gmail.com>
2025-02-26 01:10:25 -0800

    generate_page(), extract_title() and purge_contents() functions added to utils.py modified main.py to do intitial test generation from index.md in contents.

--- src/main.py
+++ src/main.py
@@ -2,8 +2,9 @@ from utils import *
 
 def main():
     print("hello world")
+    purge_contents("./public")
     copy_folder("./static", "./public")
-
+    generate_page(".\content\index.md", ".\template.html", ".\public\index.html")
 
 
 main()
--- src/utils.py
+++ src/utils.py
@@ -9,29 +9,32 @@ def markdown_to_html_node(markdown : str) -> ParentNode:
 def list_directory(path : str) -> list[str]:
     return [pathname for name in os.listdir(path) for pathname in ([os.path.join(path, name)] + list_directory(os.path.join(path, name)) if os.path.isdir(os.path.join(path, name)) else [os.path.join(path, name)])]
 
+def purge_contents(path: str):
+    if os.path.isdir(path):
+        for dir_item in list_directory(path)[::-1]:
+            if os.path.isdir(dir_item):
+                print(f"Removing folder: '{dir_item}'")
+                os.rmdir(dir_item)
+            else:
+                print(f"  Deleteing file: '{dir_item}'")
+                os.remove(dir_item)
+    else:
+        print(f"Deleteing file: '{path}'")
+        os.remove(path)
+        
 def copy_folder(source: str, destination: str):
 
     print(f"Copying from '{source}' to '{destination}':")
-    f_copy = 0, f_del = 0, d_make = 0, d_remove = 0
+    f_copy = 0
+    f_del = 0
+    d_make = 0
+    d_remove = 0
 
     if not os.path.exists(source):
         raise Exception(f"Source folder or file '{source}' does not exist.")
     
     if os.path.exists(destination):
-        if os.path.isdir(destination):
-            for dir_item in list_directory(destination)[::-1]:
-                if os.path.isdir(dir_item):
-                    print(f"Removing destination folder: '{dir_item}'")
-                    os.rmdir(dir_item)
-                    d_remove += 1
-                else:
-                    print(f"  Deleteing destination file: '{dir_item}'")
-                    os.remove(dir_item)
-                    f_del += 1
-        else:
-            print(f"Deleteing destination file: '{dir_item}'")
-            os.remove(destination)
-            f_del += 1
+        purge_contents(destination)
     else:
         print(f"Creating destination folder '{destination}'.")
         os.mkdir(destination)
@@ -40,7 +43,7 @@ def copy_folder(source: str, destination: str):
     if os.path.isfile(source):
         objects_to_copy = [(source, os.path.join(destination, os.path.basename(source)))]
     else:
-        objects_to_copy = [(path, os.path.join(destination, path.removeprefix(source).lstrip("/"))) for path in list_directory(source)]
+        objects_to_copy = [(path, os.path.join(destination, path.removeprefix(source).lstrip(os.path.pathsep))) for path in list_directory(source)]
     
     for source_object, destination_object in objects_to_copy:
         if os.path.isfile(source_object):
@@ -53,3 +56,31 @@ def copy_folder(source: str, destination: str):
             os.mkdir(destination_object)
             d_make += 1
     print(f"{f_copy} files were copied and {d_make} directories were created during the copy.")
+
+def extract_title(markdown : str) -> str:
+    title = [line.strip().removeprefix("# ").strip() for line in markdown.splitlines() if line.strip().startswith("# ")]
+    if len(title) == 0:
+        raise Exception("No title found in given markdown.")
+    else:
+        return title[0]
+
+def generate_page(from_path : str, template_path : str, dest_path: str):
+    print(f"Generating page from '{from_path}' to '{dest_path}' using '{template_path}'.")
+    if not os.path.exists(from_path) or not os.path.exists(template_path):
+        if os.path.exists(from_path):
+            raise FileNotFoundError(f"Could not find template file: '{template_path}'.")
+        else:
+            raise FileNotFoundError(f"Could not find markdown file: '{from_path}'.")
+        
+    markdown, template = (open(from_path).read(), open(template_path).read())
+    html = markdown_to_html_node(markdown).to_html()
+    html = template.replace("{{ Content }}", html).replace("{{ Title }}", extract_title(markdown))
+    
+    dest_path_list = dest_path.split(os.path.pathsep)
+    for i in range(1, len(dest_path_list)):
+        dest_sub_path = os.path.join(dest_path_list[:i])
+        if not os.path.exists(dest_sub_path):
+            os.mkdir(dest_sub_path)
+    dest_file = open(dest_path, "w")
+    dest_file.write(html)
+    dest_file.close()
